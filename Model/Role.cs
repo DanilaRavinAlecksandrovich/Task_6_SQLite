9e1bae98da50ce719f953aef28225d971aba9ab6
IRU <IRU@WIN-B2H1FLRQ82T>
2023-03-20 22:41:54 +0300

    Добавьте файлы проекта.

---
+++ Model/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6_SQLite.Model
+{
+    public class Role
+    {
+
+        [Key]
+        public int RoleID { get; set; }
+        public string RoleName { get; set; }
+
+        public virtual ICollection<User> Users { get; private set; } = new ObservableCollection<User>();
+
+    }
+}
---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task_6_SQLite.Core;
+using Task_6_SQLite.View.Pages;
+
+namespace Task_6_SQLite
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Util.UtilFrame = MainFrame;
+            MainFrame.Navigate(new LoginPage());
+        }
+
+        private void Grid_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (e.ClickCount== 2) 
+            {
+                Application.Current.Shutdown(); 
+            }
+        }
+    }
+}
